57ac60fc25c42fbfe07de471950696a9c59a9961
Rachel Bock <rachel.online@gmail.com>
2022-02-09 20:17:58 -0700

    Add patrons and variable annotations

--- challenge/javascript.js
+++ challenge/javascript.js
@@ -1,6 +1,24 @@
+// Because I will be writing in JavaScript, I chose the name of the
+// language as the file name.
+
+// String.
 var title;
+
+// String
 var author;
+
+// Could be a string or a number.
 var copyrightDate;
+
+// Number variable, lists number of pages in book.
 var numberOfPages;
+
+// Boolean type, lists whether or not the book is available to checkout.
 var checkedOut;
+
+// String type, stores name where book was checked out.
 var nameOfLibrary;
+
+// Patrons is to record who checked out the book.  Will store an array
+// of strings with patron names.
+var patrons;
